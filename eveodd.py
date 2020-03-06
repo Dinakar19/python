4801e26106cdc10a4a2880c051790be08cb0fa7b
Dinakar19 <55407860+Dinakar19@users.noreply.github.com>
2020-03-06 21:30:26 +0530

    Add files via upload

---
+++ eveodd.py
@@ -0,0 +1,7 @@
+# to find whether the number is even or odd
+a=int(input("Enter a number"))
+if a%2==0:
+   print("The given number",a,"is even number")
+else:
+   print("The given number",a,"is odd number")
+
